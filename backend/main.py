104315bebfcc492429d91e7e8112288f06d17adf
Shalini <developers@mtbs.in>
2025-04-25 14:59:10 +0530

    image path changes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.student_route import router as student_router
+import os
 
 app = FastAPI(title="Student API", version="1.0.0")
 from fastapi.staticfiles import StaticFiles
@@ -16,10 +17,11 @@ app.add_middleware(
 
 app.include_router(student_router, prefix="/api")
 # app.mount("/images", StaticFiles(directory="C:/desktop folder/py_react_tsx_todo/backend/images"), name="images")
-app.mount("/images", StaticFiles(directory="backend/images"), name="images")
+# app.mount("/images", StaticFiles(directory="backend/images"), name="images")
 
+image_dir = os.path.join(os.getcwd(), "backend/images")
 
-app.include_router(student_router, prefix="/api")
-app.mount("/images", StaticFiles(directory="backend/images"), name="images")
+app.mount("/images", StaticFiles(directory=image_dir), name="images")
+
 
 
